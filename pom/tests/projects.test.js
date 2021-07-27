f6dbe05d77a8dd0f375a9eb08ffdb889498de5e1
Elizabeth Cobián <elizabeth.cobian@wizeline.com>
2021-07-26 19:33:39 -0500

    Configuration of ESLint to have the best code standars
    In this commit I added to the repo the ESlint configuration in
    order to have and implement the best code standars

--- pom/tests/projects.test.js
+++ pom/tests/projects.test.js
@@ -1,16 +1,17 @@
-import { URLS, CREDENTIALS, PROJECT } from '../data/Constants'
+/* eslint-disable no-undef */
+import { URLS, PROJECT } from '../data/Constants'
 import appToday from '../pages/appToday'
-import  projects  from '../pages/projectsPage'
+import projects from '../pages/projectsPage'
 import { STANDAR_USER } from '../data/Roles'
 
 fixture('Projects')
-    .page (URLS.LOGIN_URL)
-    .beforeEach(async t => {
-        await t.useRole(STANDAR_USER)
-        await t.expect((appToday.todayTitle).exists).ok({timeout: 10000})
-    })
+  .page(URLS.LOGIN_URL)
+  .beforeEach(async t => {
+    await t.useRole(STANDAR_USER)
+    await t.expect((appToday.todayTitle).exists).ok({ timeout: 10000 })
+  })
 
 test('Creating a new project', async t => {
-    await projects.addProject(PROJECT.PROJECT_NAME)
-    await t.expect((projects.projectAdded).exists).ok()
+  await projects.addProject(PROJECT.PROJECT_NAME)
+  await t.expect((projects.projectAdded).exists).ok()
 })
